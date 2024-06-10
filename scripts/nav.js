8536adf241d37d39ca2428f0c1dec73561cf0a2a
clementsb <bgc0003@auburn.edu>
2024-06-10 14:09:24 -0500

    finishing changes

---
+++ scripts/nav.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('get', '/nav.html', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById("navContainer").innerHTML = xhr.responseText;
+        }
+    }
+    xhr.send();
+});
+
